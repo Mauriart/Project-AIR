9338ed0519becc6a1dd7e0c6eed420e5a6876e8d
Mauricio Art <mauriart11@gmail.com>
2026-05-20 20:35:43 -0600

    db(azure): Conexion a Base de datos en la nube: Azure

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     parseInt(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
   database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err, client, release) => {
