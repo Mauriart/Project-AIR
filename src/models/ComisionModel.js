74f82daace1f4a8ea2a278e3af5d64a7ee0855de
Ju4nj0s3MM <juanjomomo.jjmm11@gmail.com>
2026-05-29 04:04:12 -0600

    feat: crear estructura base del módulo de comisiones

---
+++ src/models/ComisionModel.js
@@ -0,0 +1,109 @@
+const db = require('../config/db');
+
+const crearComision = async (
+    nombre,
+    descripcion
+) => {
+
+    const query = `
+        INSERT INTO comision (
+            nombre,
+            descripcion
+        )
+        VALUES ($1, $2)
+        RETURNING *;
+    `;
+
+    const values = [
+        nombre,
+        descripcion
+    ];
+
+    const result =
+    await db.query(query, values);
+
+    return result.rows[0];
+};
+
+const listarComisiones = async () => {
+
+    const query = `
+        SELECT *
+        FROM comision
+        ORDER BY id_comision DESC;
+    `;
+
+    const result =
+    await db.query(query);
+
+    return result.rows;
+};
+
+const agregarIntegrante = async (
+    id_comision,
+    asambleista_id,
+    rol,
+    fecha_inicio,
+    fecha_fin
+) => {
+
+    const query = `
+        INSERT INTO integrante_comision (
+            id_comision,
+            asambleista_id,
+            rol,
+            fecha_inicio,
+            fecha_fin
+        )
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *;
+    `;
+
+    const values = [
+        id_comision,
+        asambleista_id,
+        rol,
+        fecha_inicio,
+        fecha_fin
+    ];
+
+    const result =
+    await db.query(query, values);
+
+    return result.rows[0];
+};
+
+const registrarAsistencia = async (
+    id_sesion,
+    asambleista_id,
+    estado_asistencia
+) => {
+
+    const query = `
+        INSERT INTO asistencia_sesion_comision (
+            id_sesion,
+            asambleista_id,
+            estado_asistencia
+        )
+        VALUES ($1, $2, $3)
+        RETURNING *;
+    `;
+
+    const values = [
+        id_sesion,
+        asambleista_id,
+        estado_asistencia
+    ];
+
+    const result =
+    await db.query(query, values);
+
+    return result.rows[0];
+};
+
+module.exports = {
+    crearComision,
+    listarComisiones,
+    agregarIntegrante,
+    registrarAsistencia
+};
